d8e6bb9fbbbc08d9a57a043e2b435e362264d416
Stanislav <golovachev.s@gmail.com>
2022-09-26 17:24:30 +0300

    Scan meesages

--- main.go
+++ main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
-	"os"
-
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"log"
 )
 
 func main() {
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("1975934180:AAGxLBIi3FPEjjSz_V-YA0Z24JR_gPGY1JQ"))
+	bot, err := tgbotapi.NewBotAPI("1975934180:AAGxLBIi3FPEjjSz_V-YA0Z24JR_gPGY1JQ")
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
 
 	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			bot.Send(msg)
+		}
+	}
 }
