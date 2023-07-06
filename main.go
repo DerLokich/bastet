370a61ef9354229bc8ea053744feaf3766a2f870
Stanislav <golovachev.s@gmail.com>
2023-07-06 12:56:09 +0300

    Add checking neighbors

--- main.go
+++ main.go
@@ -26,7 +26,6 @@ func main() {
 			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-			msg.ReplyToMessageID = update.Message.MessageID
 
 			bot.Send(msg)
 		}
