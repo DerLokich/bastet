0b36b9a5c794bcac6fa5307dcb10a0ae4933fce0
Stanislav <golovachev.s@gmail.com>
2023-07-06 13:08:24 +0300

    Add checking neighbors

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"log"
+	"strings"
 	"time"
 )
 
@@ -18,16 +19,18 @@ func main() {
 
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
-
+	substr := "сосед"
 	updates := bot.GetUpdatesChan(u)
 
 	for update := range updates {
+		messageText := update.Message.Text
 		if update.Message != nil { // If we got a message
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text, substr)
 
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-
-			bot.Send(msg)
+			if strings.Contains(messageText, substr) {
+				bot.Send(msg)
+			}
 		}
 		if update.Message == nil { // ignore any non-Message updates
 			continue
