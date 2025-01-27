2df4fe8a3bc4eb081a62232431434f3e82c37c0d
Golovachev.SO <Golovachev.S@gmail.com>
2025-01-27 17:45:51 +0300

    Add Start

--- main.go
+++ main.go
@@ -69,7 +69,7 @@ func main() {
 			bot.Request(kill)
 		// Этот фрагмент кода позволяет боту устанавливать определенные права доступа для указанного пользователя в чате при получении команды "iddqd"
 		case cmdStart:
-			text := "Привет и __underline__"
+			text := "*bold _italic bold ~italic bold strikethrough ||italic bold strikethrough spoiler||~ __underline italic bold___ bold*"
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, text)
 			msg.ParseMode = "MarkdownV2"
 			bot.Send(msg)
